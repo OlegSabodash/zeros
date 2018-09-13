a9027d50c243d2b4943a2fb7d5747f43c07bfe8e
unknown <o.sabodash@mail.ru>
2018-09-13 22:50:59 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var sum = 0;
+    var n = 1;
+    while (Math.pow(5, n) < number) {
+        sum += Math.floor(number / Math.pow(5, n));
+        n++;
+    }
+    return sum;
 }
